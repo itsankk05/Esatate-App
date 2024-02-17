9db7394704c4b8869cf42b02e04e0a9bafa0f5bc
Ank Agarwal <aank.ag1234@gmail.com>
2024-02-17 22:51:40 +0530

    Created API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -17,3 +18,10 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
+// app.get("/test", (req, res) => {
+//   res.json({
+//     message: "hello",
+//   });
+// });
