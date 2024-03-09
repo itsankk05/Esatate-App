2b738e71976cbb7923a077aa5f5fcce27365dbb8
Ank Agarwal <aank.ag1234@gmail.com>
2024-03-09 21:45:57 +0530

    complete delete listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -139,6 +139,24 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto text-center">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -255,7 +273,12 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center">
-                <button className="text-red-700">Delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700">Edit</button>
               </div>
             </div>
